fb00b09a383ccc6640a4c363792447950555ae73
Xppresso <jocelin.celin@gmail.com>
2026-02-17 19:05:33 +0700

    hidup jokowi

---
+++ Scripts/menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var transition = $Transition
+var is_playing = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_new_game_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+	
+func _on_exit_pressed() -> void:
+	get_tree().quit()
