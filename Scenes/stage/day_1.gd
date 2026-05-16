0d09f3fb1770c0a008440c0e2a59c5eacb0b5208
Xppresso <jocelin.celin@gmail.com>
2026-05-16 14:12:36 +0700

    characters

---
+++ Scenes/stage/day_1.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const MALWARE_PUZZLE := preload("res://Scenes/puzzle/malware_puzzle/malware.tscn")
+
+const TIMELINE_HG_WEEK1 := "highschooler_week1"
+const TIMELINE_HG_WEEK1_POST := "highschooler_week1_post"
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	pass
+	# Dialogic.start(TIMELINE_HG_WEEK1)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
